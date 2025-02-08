7f8455a8ff27ec164270d4531f4c110dbeff3131
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-02-08 17:00:44 +0200

    Basic navigation added

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap;
+          if (route.name === "(plants)") {
+            iconName = "leaf";
+        } else if (route.name === "profile") {
+            iconName = "person";
+        } else if (route.name === "settings") {
+            iconName = "settings";
+        } else {
+            iconName = "ellipse";
+        }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#000000",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+      })}
+    >
+      <Tabs.Screen name="(plants)" options={{ title: "Plants" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen name="settings" options={{ title: "Settings" }} />   
+    </Tabs>
+  );
+}
