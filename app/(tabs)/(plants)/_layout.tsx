8752ee6adda311df4ee00c309cfa530df17f860b
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-02-09 11:40:25 +0200

    prettier for layout

--- app/(tabs)/(plants)/_layout.tsx
+++ app/(tabs)/(plants)/_layout.tsx
@@ -4,11 +4,11 @@ import { PlantProvider } from "./plantContext/PlantContext";
 import { useColorScheme } from "react-native";
 
 export default function PlantsLayout() {
-    const colorScheme = useColorScheme();
-    
-      const themeBackgroundStyle = colorScheme === "light" ? "#949D6A" : "#5A6340";
-      const themeTextStyle = colorScheme === "light" ? "#F1EDEE" : "#D7CDCC";
-      
+  const colorScheme = useColorScheme();
+
+  const themeBackgroundStyle = colorScheme === "light" ? "#949D6A" : "#5A6340";
+  const themeTextStyle = colorScheme === "light" ? "#F1EDEE" : "#D7CDCC";
+
   return (
     <PlantProvider>
       <Stack
